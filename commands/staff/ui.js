7cfae2c6af76df3461bae1fbb67a7075998a21df
Its0xyToan <106913620+Its0xyToan@users.noreply.github.com>
2022-06-26 19:14:27 -0400

    Userinfo command
    Montre des infos par rapport a l'utilisateur mentionné, created date joined date, id , nom...

---
+++ commands/staff/ui.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js"); 
+module.exports = (message, args) { 
+  
+         var embed = new Discord.MessageEmbed() 
+         .setColor('#0099ff') 
+         .setDescription(`Information about ${message.author.username}#${message.author.discriminator}`) 
+         .setTitle('User Info') 
+         .addField('Username', `${message.author.username}`) 
+         .addField('ID', `||${message.author.id}||`) 
+         .addField('Created At', `${message.author.createdAt}`) 
+         .addField('Joined At', `${message.member.joinedAt}`) 
+         .setThumbnail(`${message.author.avatarURL()}`) 
+         .setFooter(`Requested by ${message.author.username}#${message.author.discriminator}`) 
+  
+  
+         .setTimestamp() 
+  
+         message.channel.send({embeds: [embed]}); 
+  
+     } 
+ }
